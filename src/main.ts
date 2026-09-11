82e23b23dbf125e1db0d26208e1615cc408454fd
antuaan23 <ni.antugb@gmail.com>
2026-09-11 17:09:10 -0300

    Fix: Especificamos host en el main.ts para que fly.io pueda deployar por el host 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -32,11 +32,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 
-  console.log(`🚀 Aplicación corriendo en: http://localhost:${port}/api`);
-  console.log(`📚 Documentación Swagger disponible en: http://localhost:${port}/api/docs`);
 }
 
 bootstrap();
